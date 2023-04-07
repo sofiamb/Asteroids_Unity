b05ac86e3755fce631f3d2da8658250514611313
sofiamb <sofiambombelli@gmail.com>
2023-04-07 04:35:22 -0300

    Asteroids movement

--- Ateroids_Bombelli/Assets/Scripts/Asteroid.cs
+++ Ateroids_Bombelli/Assets/Scripts/Asteroid.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Speed Settings")]
+    public float speed;
+    private Rigidbody2D AsteroidRB;
+
     void Start()
     {
-        
+        AsteroidRB = GetComponent<Rigidbody2D>();
+        AsteroidRB.drag = 0;
+        AsteroidRB.angularDrag = 0;
+
+        AsteroidRB.velocity = new Vector3(Random.Range(-1,1), Random.Range(-1, 1), 0).normalized * speed;
+        AsteroidRB.angularVelocity = Random.Range(-50, 50);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
